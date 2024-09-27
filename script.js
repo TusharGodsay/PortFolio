5fe40167f6deea6bd332b5a0d3576fa5a4159d6d
Tushar Godsay <Tushargodsay001@gmail.com>
2024-09-27 16:06:20 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+// let arr = ['tushar',5,'dass']
+// let index = arr.indexOf('dass')
+// arr[10] = 'God Is Kabir Saheb';
+// console.log(index);
+// console.log(arr.length);//length utni hogi jitne ka hum array lenge jaise maine arr[10] liya eska length 10/11 hogi
+// console.log(Object.keys(arr));//esme sirf jo assgin hai vohoi print honge
+// console.log(arr[8]);//undefind aayge jo bich me assign na kiye gye ho
+// arr.push("godsay","kumar",7,5);
+// function hire(){
+//     gmail = prompt("Enter Your Company Gmail : ")
+//     alert(" Email Was sent You to after some time at " +gmail)
+//     alert("Thanks You");
+
+// }
+// name = prompt('Enter value of n');
+// let sum = 0;
+// for(let i =0;i<=name;i++){
+//     sum+=i;
+//     console.log("sum is "+sum );
+// }
