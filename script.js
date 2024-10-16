30330e82404a14a1696ca06cc320b6a39a189927
Tushar Godsay <Tushargodsay001@gmail.com>
2024-10-16 21:14:38 +0530

    all done till education

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
-// let arr = ['tushar',5,'dass']
-// let index = arr.indexOf('dass')
-// arr[10] = 'God Is Kabir Saheb';
-// console.log(index);
-// console.log(arr.length);//length utni hogi jitne ka hum array lenge jaise maine arr[10] liya eska length 10/11 hogi
-// console.log(Object.keys(arr));//esme sirf jo assgin hai vohoi print honge
-// console.log(arr[8]);//undefind aayge jo bich me assign na kiye gye ho
-// arr.push("godsay","kumar",7,5);
-// function hire(){
-//     gmail = prompt("Enter Your Company Gmail : ")
-//     alert(" Email Was sent You to after some time at " +gmail)
-//     alert("Thanks You");
 
-// }
-// name = prompt('Enter value of n');
-// let sum = 0;
-// for(let i =0;i<=name;i++){
-//     sum+=i;
-//     console.log("sum is "+sum );
-// }
+    const menuBtn = document.querySelector('.menu-btn');
+    const ul = document.querySelector('#ul');
+    const body = document.body;
+
+    menuBtn.addEventListener('click', function() {
+        if (ul.style.left === '0px') {
+            ul.style.left = '-100%';
+            ul.style.opacity = '0';
+            body.classList.remove('menu-open'); // remove class to enable scroll
+        } else {
+            ul.style.left = '0';
+            ul.style.opacity = '1';
+            body.classList.add('menu-open'); // add class to disable scroll
+        }
+    });
+    
+    //check github (changes) && Updates till Education (without include images and more info but )
