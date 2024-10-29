ce60a34852fa4ecec0e6e94967cdc7cd5b02d71a
Tushar Godsay <Tushargodsay001@gmail.com>
2024-10-29 22:44:45 +0530

    ul and navbar color

--- script.js
+++ script.js
@@ -2,30 +2,27 @@
     const menuBtn = document.querySelector('.menu-btn');
     const ul = document.querySelector('#ul');
     const body = document.body;
-
+    const Navbar = document.querySelector('#navbar')
     menuBtn.addEventListener('click', function() {
         if (ul.style.left === '0px') {
             ul.style.left = '-100%';
             ul.style.opacity = '0';
             body.classList.remove('menu-open'); // remove class to enable scroll
+            Navbar.classList.remove('nav-open')
+
         } else {
             ul.style.left = '0';
             ul.style.opacity = '1';
             body.classList.add('menu-open'); // add class to disable scroll
+            Navbar.classList.add('nav-open')
         }
     });
-    
-    //check github (changes) && Updates till Education (without include images and more info but )
     function confirmDownload() {
-        // Confirm download
         const userConfirmed = confirm("Do you want to download My CV?");
     
         if (userConfirmed) {
-            // User clicked OK, start the download
             window.location.href = './Assets/images/Resume.pdf';
         } else {
-            // User clicked Cancel
             alert("Download cancelled.");
         }
     }
-    
